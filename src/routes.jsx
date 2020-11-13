092426695f270d893e95c34f8b5c3f3faa9b4999
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-11-13 18:27:33 -0300

    create login page with form to login

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './Pages/Login';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route />
+      <Route exact path="/" component={Login} />
     </Switch>
   </BrowserRouter>
 );

b5ce36d89a7ea221670a4ba86a129d1ec6f3a08f
danwhat <62217873+danwhat@users.noreply.github.com>
2020-11-14 10:21:26 -0300

    Update API-gravatar.js
    Arrumando o nome da função para ficar digdin conforme o  padrão de projeto.

--- src/Services/API/API-gravatar.js
+++ src/Services/API/API-gravatar.js
@@ -1,7 +1,7 @@
 import md5 from 'crypto-js/md5';
 
-// gravatarImg(emailGravatar) retorna url da imagem
-export const gravatarImg = (email) => {
+// gravatarImgUrlGet(emailGravatar) retorna url da imagem
+export const gravatarImgUrlGet = (email) => {
     const hash = md5(email)
     const hashString = hash.toString()
     return `https://www.gravatar.com/avatar/${hashString}`

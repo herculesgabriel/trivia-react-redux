23e94b877c74d070df385dea52edb3abf4d2a7b8
Luiz Filipe <lfsmariz@gmail.com>
2020-11-15 14:07:50 -0300

    API gravatar update indentation

--- src/Services/API/API-gravatar.js
+++ src/Services/API/API-gravatar.js
@@ -1,9 +1,8 @@
-import md5 from 'crypto-js/md5';
+import md5 from "crypto-js/md5";
 
 // gravatarImgUrlGet(emailGravatar) retorna url da imagem
-export const gravatarImgUrlGetter= (email) => {
-    const hash = md5(email)
-    const hashString = hash.toString()
-    return `https://www.gravatar.com/avatar/${hashString}`
-}
-
+export const gravatarImgUrlGetter = (email) => {
+  const hash = md5(email);
+  const hashString = hash.toString();
+  return `https://www.gravatar.com/avatar/${hashString}`;
+};
